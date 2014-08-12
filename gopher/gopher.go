aff6ca2fdb163af76d4244fe7d1707ea141099fe
Yaruson <nikolay.kim@outlook.com>
2014-08-12 07:58:53 +0400

    Refactoring step
    Don't work :crying_cat_face:

--- gopher/gopher.go
+++ gopher/gopher.go
@@ -11,111 +11,120 @@ import "io/ioutil"
 
 import "github.com/catfriends/simplegopherd/configuration"
 
-
 const (
-  EDirectory = "1"
-  EError     = "3"
-  EInfo      = "i"
-  EBinary    = "9"
+	EDirectory = "1"
+	EError     = "3"
+	EInfo      = "i"
+	EBinary    = "9"
 )
 
-var extensionType = map[string]string {
-  ".txt": "0",
-  ".diz": "0",
-  ".nfo": "0",
-  ".gif": "g",
-  ".jpg": "I",
-  ".jpeg": "I",
-  ".png": "I",
-  ".mp3": "s",
-  ".wav": "s",
-  ".mid": "s",
+var extensionType = map[string]string{
+	".txt":  "0",
+	".diz":  "0",
+	".nfo":  "0",
+	".gif":  "g",
+	".jpg":  "I",
+	".jpeg": "I",
+	".png":  "I",
+	".mp3":  "s",
+	".wav":  "s",
+	".mid":  "s",
 }
 
 const empty = ""
 const nl = "\n"
 
-func ProcessRequest(selector string) ([]byte) {
-  log.Printf("Processing request [%s]", selector)
-  if selector == empty {
-    return HandleSelector(configuration.BaseDirectory())
-  } else {
-    return HandleSelector(selector)
-  }
+// Process Gopher request from the client.
+func ProcessRequest(selector string) []byte {
+	if selector == empty {
+		return HandleSelector(configuration.BaseDirectory())
+	} else {
+		return HandleSelector(selector)
+	}
 }
 
+// Process selector given by request.
+func HandleSelector(selector string) []byte {
+	var fsObject string
+
+	// Find name of the filesystem object to be processed
+	if selector == empty {
+		fsObject = configuration.BaseDirectory()
+	} else {
+		fsObject = path.Join(configuration.BaseDirectory(), selector)
+	}
+
+	// Get information about the filesystem object
+	if stat, e := os.Stat(fsObject); e != nil {
+		return []byte(gopherError(e.Error()))
+	} else {
+		if stat.IsDir() == true {
+			return ReadIndex(fsObject)
+		} else {
+			return ReadFile(fsObject)
+		}
+	}
+}
+
+// Find and process Index file for specified directory.
 func ReadIndex(referenceDir string) ([]byte) {
-  log.Printf("Reading index of [%s]", referenceDir)
-  if f, e := os.Open(path.Join(referenceDir, configuration.IndexFileName())); e != nil {
-    return []byte(gopherError(e.Error()))
-  } else {
-    defer f.Close()
-    
-    index := make([]string, 0)
+	if f, e := os.Open(path.Join(referenceDir, configuration.IndexFileName())); e != nil {
+		return []byte(gopherError(e.Error()))
+	} else {
+		defer f.Close()
 
-    reader := csv.NewReader(f)
-    if lines, e := reader.ReadAll(); e != nil {
-      return []byte(gopherError(e.Error()))
-    } else {
-      for _, line := range(lines) {
-        index = append(index, indexEntry(line[0], referenceDir, line[1]))
-      }
-    }
+		index := make([]string, 0)
 
-    return []byte(strings.Join(index, nl) + nl)
+		reader := csv.NewReader(f)
+		if lines, e := reader.ReadAll(); e != nil {
+			return []byte(gopherError(e.Error()))
+		} else {
+			for _, line := range lines {
+				index = append(index, indexEntry(line[0], referenceDir, line[1]))
+			}
+		}
 
-  }
+		return []byte(strings.Join(index, nl) + nl)
+
+	}
 }
 
-func HandleSelector(selector string) ([]byte) {
-  var dirPath string
-  if selector != configuration.BaseDirectory() {
-    dirPath = path.Join(configuration.BaseDirectory(), selector)
-  } else {
-    dirPath = selector
-  }
-  if stat, e := os.Stat(dirPath); e != nil {
-    return []byte(gopherError(e.Error()))
-  } else {
-    if stat.IsDir() == true {
-      return ReadIndex(dirPath)
-    } else {
-      if data, e := ioutil.ReadFile(path.Join(configuration.BaseDirectory(), selector)); e != nil {
-        return []byte(gopherError(e.Error()))
-      } else {
-        return data[:];
-      }
-    }
-  }
+// Read file in binary mode.
+func ReadFile(name string) ([]byte) {
+	if data, e := ioutil.ReadFile(name); e != nil {
+		return []byte(gopherError(e.Error()))
+	} else {
+		return data[:]
+	}
 }
 
-func gopherEntry(etype, title, url string) (string) {
-  return fmt.Sprintf("%s%s\t%s\t%s\t%s", etype, title, url, configuration.HostName(), configuration.PortNumber())
+func gopherEntry(etype, title, url string) string {
+	return fmt.Sprintf("%s%s\t%s\t%s\t%s", etype, title, url, configuration.HostName(), configuration.PortNumber())
 }
 
-func gopherError(reason string) (string) {
-  return gopherEntry(EError, reason, "")
+func gopherError(reason string) string {
+	return gopherEntry(EError, reason, "")
 }
 
-func indexEntry(title, referenceDir, selector string) (string) {
-  if selector == empty {
-    return gopherEntry(EInfo, title, empty)
-  } else {
-    if stat, e := os.Stat(path.Join(referenceDir, selector)); e != nil {
-      log.Printf("Error: %s", e.Error())
-      return gopherError(e.Error())
-    } else {
-      if stat.IsDir() == true {
-        return gopherEntry(EDirectory, title, selector)
-      } else {
-        replacer := strings.NewReplacer(configuration.BaseDirectory(), empty)
-        entrySelector := replacer.Replace(path.Join(referenceDir, selector))
-        if extensionType, e := extensionType[strings.ToLower(filepath.Ext(selector))]; e != true {
-          return gopherEntry(EBinary, title, entrySelector)
-        } else {
-          return gopherEntry(extensionType, title, entrySelector)
-        }
-      }
-    }
-  }
+func indexEntry(title, referenceDir, selector string) string {
+	if selector == empty {
+		return gopherEntry(EInfo, title, empty)
+	} else {
+		if stat, e := os.Stat(path.Join(referenceDir, selector)); e != nil {
+			log.Printf("Error: %s", e.Error())
+			return gopherError(e.Error())
+		} else {
+			if stat.IsDir() == true {
+				return gopherEntry(EDirectory, title, selector)
+			} else {
+				replacer := strings.NewReplacer(configuration.BaseDirectory(), empty)
+				entrySelector := replacer.Replace(path.Join(referenceDir, selector))
+				if extensionType, e := extensionType[strings.ToLower(filepath.Ext(selector))]; e != true {
+					return gopherEntry(EBinary, title, entrySelector)
+				} else {
+					return gopherEntry(extensionType, title, entrySelector)
+				}
+			}
+		}
+	}
 }
--- simplegopherd.go
+++ simplegopherd.go
@@ -1,48 +1,57 @@
 package main
 
 import (
-  "bufio"
-  "flag"
-  "fmt"
-  "log"
-  "net"
-  "strings"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
 
-  "github.com/catfriends/simplegopherd/configuration"
-  "github.com/catfriends/simplegopherd/gopher"
+	"github.com/catfriends/simplegopherd/configuration"
+	"github.com/catfriends/simplegopherd/gopher"
+)
+
+var (
+	configFile = flag.String("config", "", "Configuration file name")
 )
 
 func main() {
-  configurationFileName := flag.String("config", "", "INI configuration file name")
-  flag.Parse()
 
-  if e := configuration.LoadFromFile(*configurationFileName); e != nil {
-    log.Fatal(e)
-  }
+	// Load configuration from file specified
 
-  if lsck, e := net.Listen("tcp", configuration.Binding()); e != nil {
-    log.Fatal(fmt.Sprintf("Can't start server: %s", e.Error()))
-  } else {
-    log.Printf("Gopher is now up at %s", configuration.Binding())
-    for {
-      if conn, e := lsck.Accept(); e != nil {
-        log.Printf("Can't serve connection: %s", e.Error())
-      } else {
-        log.Printf("Accepting client %s", conn.RemoteAddr().String())
-        go HandleRequest(conn)
-      }
-    }
-  }
+	flag.Parse()
+	if *configFile == "" {
+		log.Fatal("You must specify config file name using -config=<file.ini>")
+	} else {
+		if e := configuration.LoadFromFile(*configFile); e != nil {
+			log.Fatal(fmt.Sprintf("Can't load configuration from %s: %s", *configFile, e.Error()))
+		}
+	}
+
+	// Create server instance
+
+	if lsck, e := net.Listen("tcp", configuration.Binding()); e != nil {
+		log.Fatal(fmt.Sprintf("Can't start: %s", e.Error()))
+	} else {
+		for {
+			if conn, e := lsck.Accept(); e != nil {
+				log.Printf("Can't serve client: %s", e.Error())
+			} else {
+				go HandleRequest(conn)
+			}
+		}
+	}
 
 }
 
 func HandleRequest(sck net.Conn) {
-  defer sck.Close()
+	defer sck.Close()
 
-  if selector, e := bufio.NewReader(sck).ReadString('\n'); e != nil {
-    log.Printf("Can't get selector string: %s", e.Error())
-  } else {
-    sck.Write(gopher.ProcessRequest(strings.Trim(selector, "\n\r\t ")))
-  }
+	if selector, e := bufio.NewReader(sck).ReadString('\n'); e != nil {
+		log.Printf("Can't get selector string: %s", e.Error())
+	} else {
+		sck.Write(gopher.ProcessRequest(strings.Trim(selector, "\n\r\t ")))
+	}
 
 }
